fd308584de19cb640aab194b64d1b58f83e330af
Estriel <fred64nls@hotmail.fr>
2021-08-29 16:41:23 +0900

    test build 5

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from test build 4</h1>
+        <h1>Hello from test build 5</h1>
       </header>
     </div>
   );
