17989cace2108ad3adb091947489578e2a829e05
Estriel <fred64nls@hotmail.fr>
2021-08-29 16:29:59 +0900

    test build 3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
+        <h1>Hello from test build 3</h1>
       </header>
     </div>
   );
